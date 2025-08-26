fc883e35309f2f102180fab7689ba77da80e4027
Adi <ripedown1@gmail.com>
2025-08-26 19:35:04 +0530

    chore: project setup with config file and main app (#34)
    * chore: project setup with config file and main app
    
    * style: ✨ auto-format with Ruff
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from cinemix!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+# from src.config import settings
 
+# Router imports
+from src.routes import search
 
-if __name__ == "__main__":
-    main()
+# App intialization
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Setup routers
+app.include_router(search.router, prefix="/api/v1")
