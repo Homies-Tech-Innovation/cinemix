04b810237706dbb66de054fd137a8ad4600869af
Adi <ripedown1@gmail.com>
2025-08-27 12:03:17 +0530

    Add env variables for all components and validations

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-# from src.config import settings
+from src.config import settings
 
 # Router imports
 from src.routes import search
