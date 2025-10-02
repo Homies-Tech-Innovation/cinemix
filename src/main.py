5c62c9dbe1e9ebac3422f4383103d661921d4dcc
Daisuke <219789162+Daisukekambe481@users.noreply.github.com>
2025-10-02 10:13:16 +0400

    redis connection setup (#43)
    * Wired up the redis
    
    * style: ✨ auto-format with Ruff
    
    * feat: implement Redis health check on startup and add redis dependency
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Adi <ripedown1@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.config import settings
@@ -7,17 +8,30 @@ from src.routes import search
 
 # logger imports
 from src.utils.logger import setup_logging
+from src.utils import logger
+
+# Redis client
+from src.services import redis_client
 
 
 # Initialize logger
-from src.utils import logger
+setup_logging()
 
-# App intialization
-app = FastAPI()
+
+# Run Redis health check on startup
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await redis_client.check_connection()
+    yield
+    await redis_client.client.close()
+
+
+# App initialization
+app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],  # or use settings.origins if you want strict CORS
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
---
+++ src/services/__init__.py
@@ -0,0 +1,1 @@
+from src.services.redis_connection import redis_client
