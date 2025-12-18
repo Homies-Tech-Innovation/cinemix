8973c9868b4d087a046ceae8fd0e54a720b662a2
monsurcodes <monsurcodes@gmail.com>
2025-12-18 17:24:38 +0530

    feat: implement search movies by title functionality

--- src/routes/search.py
+++ src/routes/search.py
@@ -1,13 +1,18 @@
 from fastapi import APIRouter
-from src.utils import get_movie
+from src.utils import get_movie, get_search_data
 from fastapi.responses import JSONResponse
 
 router = APIRouter(prefix="/search", tags=["search"])
 
 
-@router.get("/")
-def search_movies():
-    return {"message": "search movie route"}
+@router.get("/{query}")
+async def search_movies(query: str):
+    response_model = await get_search_data(query)
+    http_status_code = 200 if response_model and response_model.status == "success" else 400
+
+    return JSONResponse(
+        content=response_model.data, status_code=http_status_code
+    )
 
 
 @router.get("/id/{id}")
@@ -16,5 +21,5 @@ async def get_movie_details(id: str):
     http_status_code = 200 if response_model.status == "success" else 400
 
     return JSONResponse(
-        content=response_model.model_dump_json(), status_code=http_status_code
+        content=response_model.data, status_code=http_status_code
     )
