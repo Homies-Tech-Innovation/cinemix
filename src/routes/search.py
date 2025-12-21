c413826103b1da339816e26f198d4f9bb237a3db
monsurcodes <monsurcodes@gmail.com>
2025-12-21 13:19:23 +0530

    fix: update search_movies function to use ResponseStatus for consistency

--- src/routes/search.py
+++ src/routes/search.py
@@ -1,14 +1,15 @@
 from fastapi import APIRouter
 from src.utils import get_movie, get_search_data
+from src.models import ResponseStatus
 from fastapi.responses import JSONResponse
 
 router = APIRouter(prefix="/search", tags=["search"])
 
 
 @router.get("/{query}")
-async def search_movies(query: str):
-    response_model = await get_search_data(query)
-    http_status_code = 200 if response_model and response_model.status == "success" else 400
+def search_movies(query: str):
+    response_model = get_search_data(query)
+    http_status_code = 200 if response_model and response_model.status == ResponseStatus.SUCCESS else 400
 
     return JSONResponse(
         content=response_model.data, status_code=http_status_code
@@ -18,7 +19,7 @@ async def search_movies(query: str):
 @router.get("/id/{id}")
 async def get_movie_details(id: str):
     response_model = await get_movie(id)
-    http_status_code = 200 if response_model.status == "success" else 400
+    http_status_code = 200 if response_model.status == ResponseStatus.SUCCESS else 400
 
     return JSONResponse(
         content=response_model.data, status_code=http_status_code
