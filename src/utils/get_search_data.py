8b4b0589cc3740f780c63aa26ac8948aa745269c
monsurcodes <monsurcodes@gmail.com>
2025-12-21 13:19:53 +0530

    fix: change get_search_data to a synchronous function

--- src/utils/get_search_data.py
+++ src/utils/get_search_data.py
@@ -1,7 +1,7 @@
 from src.services import omdb_client
 from src.utils import response_parser, Endpoint, logger
 
-async def get_search_data(query: str):
+def get_search_data(query: str):
     data, status_code = omdb_client.fetch_search(movie_title=query)
     
     return response_parser.parse_response(data, status_code, Endpoint.SEARCH)
